4a3cdba264f037df122303daca8679f8f5be63d4
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-02-10 20:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	var form = document.getElementById('form1');
 
+	var firstname = form.elements["fname"].value;
+	var lastname = form.elements["lname"].value;
+	alert(firstname + " " + lastname);
 }
